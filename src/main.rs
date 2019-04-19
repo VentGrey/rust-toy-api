0d10a350af5123ab2a1b57e3f4df39700cb2f09d
VentGrey <omarpurataf@gmail.com>
2019-04-19 05:02:21 -0500

    Added connections to database

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
 
 mod models;
 mod schema;
 
 fn main(){
     dotenv().ok(); // Because LAZY ERROR HANDLER
+
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    let conn = PgConnection::establish(&database_url).unwrap();
+
+
 }
