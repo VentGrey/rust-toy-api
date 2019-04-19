e56c99d582c8b96a7e0f67d3bfbfb11ae57213ea
VentGrey <omarpurataf@gmail.com>
2019-04-19 00:31:51 -0500

    Auto .lock file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+mod schema;
+mod models;
+
+fn main(){
+    println!("Hello mundo");
 }
