39f24ea9e8b3f13ce62522ecbd391d68d73cc36a
VentGrey <omarpurataf@gmail.com>
2019-04-19 20:19:27 -0500

    Added init pool function

--- src/db.rs
+++ src/db.rs
@@ -4,3 +4,10 @@ use r2d2_diesel::ConnectionManager;
 use rocket::request::{self, FromRequest};
 use rocket::{Outcome, Request, State};
 use std::ops::Deref;
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+pub fn init_pool(db_url: String) -> Pool {
+    let manager = ConnectionManager::<PgConnection>::new(db_url);
+    r2d2::Pool::new(manager).expect("db pool failure")
+}
