3d95dddd48b39cc8e55c4baa07c6babc89fc0358
VentGrey <omarpurataf@gmail.com>
2019-04-19 22:57:56 -0500

    added json parser

--- src/routes.rs
+++ src/routes.rs
@@ -1,2 +1,14 @@
 use db::Conn as DbConn
 use rocket_contrib::Json;
+use super::models::{Book, NewBook};
+use serde_json::Value;
+
+#[get("/books", format = "application/json")]
+fn index(conn: DbConn) -> Json<Value> {
+    let books = Book::all(&conn);
+
+    Json(json!({
+        "status": 200,
+        "result": books,
+    }))
+}
