ff9065a6f2c52fc3190a087243d4d8ab25746eac
VentGrey <omarpurataf@gmail.com>
2019-04-19 19:52:39 -0500

    Added more macros just in case

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,16 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
 extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate rocket;
+extern crate rocket_contrib;
 
+#[macro_use]
+extern crate serde_derive;
 
+#[macro_use]
+extern crate serde_json;
 
 use dotenv::dotenv;
 use std::env;
