d4fac72aee5e7fdf18e1d4aafb8fb38af8e48b8e
VentGrey <omarpurataf@gmail.com>
2019-04-20 22:03:17 -0500

    added functions

--- app/elm/main.elm
+++ app/elm/main.elm
@@ -7,9 +7,31 @@ import Http
 import Json.Decode as Json
 import Json.Encode as Encode
 
+
 type alias Book =
     { title : String
     , author : String
     , published : Bool
     }
 
+type alias Model =
+    {books : List Book}
+
+
+
+init =
+    ( Model [], Cmd.none )
+
+type Msg
+    = GetBooks (Result Http.Error (List Book))
+    | RequestBooks
+
+update: Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+        case msg of
+            GetBooks (Ok json) ->
+                ( { model | books = json  }, Cmd.none )
+            GetBooks (Err e) ->
+                ( Debug.log (toString e) model, Cmd.none )
+            RequestBooks ->
+                (model, getBooks)
